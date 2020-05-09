5353501d9dbdbdd1e2919f7848822910588efa7e
Sang2306 <sangvnorg@gmail.com>
2020-05-09 22:55:22 +0700

    Sử dụng card view trong list view để hiển thị các item

--- MyBlog/app/src/main/java/com/example/myblog/MainActivity.java
+++ MyBlog/app/src/main/java/com/example/myblog/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.myblog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView articleListView;
+    private ImageButton searchBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setControl();
+        initListViewItems();
+    }
+
+    private void setControl(){
+        articleListView = findViewById(R.id.articleListView);
+        searchBtn = findViewById(R.id.searchBtn);
+    }
+
+    private void setEvent(){
+
+    }
+
+    private void initListViewItems(){
+        String[] articles = {"Model serialization notes", "How to use Django REST effectively"};
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.article_listview, R.id.articleTextView, articles);
+        articleListView.setAdapter(arrayAdapter);
     }
 }
